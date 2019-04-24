e747e6e56c843ecc344396dff2faa1b8381602a4
Derek Brown <derek.brown@datadoghq.com>
2019-04-24 11:32:57 -0700

    Add creation of bin/agent

--- omnibus/config/software/datadog-agent-prepare.rb
+++ omnibus/config/software/datadog-agent-prepare.rb
@@ -24,7 +24,7 @@ end
 if windows?
   build do
     block do
-      FileUtils.mkdir_p(File.expand_path(File.join(install_dir, "bin")))
+      FileUtils.mkdir_p(File.expand_path(File.join(install_dir, "bin", "agent")))
     end
   end
 end
